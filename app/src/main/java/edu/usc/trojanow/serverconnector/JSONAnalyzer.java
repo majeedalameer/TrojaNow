f9b48244185dc3a301f5b4648c914f09366a1cae
sadeemsaleh <sadeem.alsudais@gmail.com>
2015-04-30 14:22:30 -0700

    server connector

--- app/src/main/java/edu/usc/trojanow/serverconnector/JSONAnalyzer.java
+++ app/src/main/java/edu/usc/trojanow/serverconnector/JSONAnalyzer.java
@@ -1,15 +1,135 @@
 package edu.usc.trojanow.serverconnector;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import edu.usc.trojanow.location.LocationInfo;
+import edu.usc.trojanow.messages.DirectMessage;
+import edu.usc.trojanow.sensor.TemperatureInfo;
+import edu.usc.trojanow.thought.Thought;
+import edu.usc.trojanow.user.User;
+
 /**
  * Created by abdulmajeed on 3/24/15.
  */
 public class JSONAnalyzer {
 
+
+    public static User AnalyzeJsonUser(String json){
+User user=null;
+        try {
+            JSONArray object = new JSONArray(json);
+            String username,firstName,lastName;
+            for(int i=0;i<object.length();i++)
+            {
+                JSONObject obj = object.getJSONObject(i);
+                username=obj.getString("username");
+                firstName=obj.getString("name");
+                lastName=obj.getString("name");
+                user=new User(username,firstName,lastName,null);
+            }
+        }
+        catch (JSONException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        return user;
+    }
+
     // this method analyze a JSON string and return the corresponding object
-    public static Object AnalyzeJson(String json){
-        //TODO: fill in method skeleton
-        return null;
+    public static ArrayList<Thought> AnalyzeJson(String json){
+        ArrayList<Thought> arrayList = new ArrayList<>();
+        try {
+            JSONArray object = new JSONArray(json);
+            Thought thought=null;
+            String text;
+            char emptyChar='\0';
+            double longit,lat;
+            LocationInfo location;
+            TemperatureInfo temprature;
+            User user;
+            for(int i=0;i<object.length();i++)
+            {
+                JSONObject obj = object.getJSONObject(i);
+                text=obj.getString("text");
+                longit=Double.parseDouble(obj.getString("long"));
+                lat=Double.parseDouble(obj.getString("lat"));
+                location =new LocationInfo(longit,lat,null);
+                temprature=new TemperatureInfo(Float.parseFloat(obj.getString("temp")),emptyChar,null);
+                user=new User(obj.getString("user"),obj.getString("user"),obj.getString("user"),null);
+                thought=new Thought(text,location,temprature,user);
+                arrayList.add(thought);
+            }
+        }
+        catch (JSONException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        return arrayList;
     }
 
 
+
+    public static ArrayList<DirectMessage> AnalyzeJsonMsg(String json){
+        ArrayList<DirectMessage> arrayList = new ArrayList<>();
+        try {
+            JSONArray object = new JSONArray(json);
+            DirectMessage msg=null;
+            String text,date;
+            User sender,reciever;
+            for(int i=0;i<object.length();i++)
+            {
+                JSONObject obj = object.getJSONObject(i);
+                text=obj.getString("text");
+                date=obj.getString("date");
+                sender=new User(obj.getString("sender"),obj.getString("sender"),obj.getString("sender"),null);
+                reciever=new User(obj.getString("reciever"),obj.getString("reciever"),obj.getString("reciever"),null);
+                msg=new DirectMessage(sender,reciever,text,null);
+                arrayList.add(msg);
+            }
+        }
+        catch (JSONException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        return arrayList;
+    }
+
+
+    public static ArrayList<NameValuePair> composeJsonLogin(String username)
+    {
+
+        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair("username",username));
+        return nameValuePairs;
+    }
+
+
+public static ArrayList<NameValuePair> composeJson(Thought thought)
+{
+
+    ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+    nameValuePairs.add(new BasicNameValuePair("user",thought.getCreatedBy().getUserName()));
+    nameValuePairs.add(new BasicNameValuePair("text",thought.getText()));
+    nameValuePairs.add(new BasicNameValuePair("anony","false"));
+    nameValuePairs.add(new BasicNameValuePair("temp","33"));
+    nameValuePairs.add(new BasicNameValuePair("long",thought.getLocation().getLongitude()+""));
+    nameValuePairs.add(new BasicNameValuePair("lat","32"));
+    return nameValuePairs;
+}
+    public static ArrayList<NameValuePair> composeJsonMsg(DirectMessage msg)
+    {
+
+        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair("reciever",msg.getReceiver().getUserName()));
+        nameValuePairs.add(new BasicNameValuePair("sender",msg.getSender().getUserName()));
+        nameValuePairs.add(new BasicNameValuePair("text",msg.getMessageText()));
+        nameValuePairs.add(new BasicNameValuePair("date","12/12/12"));
+        return nameValuePairs;
+    }
 }
--- app/src/main/java/edu/usc/trojanow/serverconnector/ServerConnector.java
+++ app/src/main/java/edu/usc/trojanow/serverconnector/ServerConnector.java
@@ -1,5 +1,29 @@
 package edu.usc.trojanow.serverconnector;
 
+import android.os.StrictMode;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+
 import edu.usc.trojanow.location.LocationInfo;
 import edu.usc.trojanow.messages.DirectMessage;
 import edu.usc.trojanow.thought.Thought;
@@ -11,41 +35,143 @@ import edu.usc.trojanow.user.User;
  * Created by abdulmajeed on 3/24/15.
  */
 public class ServerConnector {
-    private String connectionString;
+    private String connectionString="http://10.123.50.232/trojanNow/";
 
     //this method posts a new thought to the server
-    public boolean postThought(Thought thought){
-        //TODO: fill in method skeleton
-        return false;
+    public void postThought(Thought thought){
+
+        InputStream is = null;
+        // http post
+        ArrayList<NameValuePair> nameValuePairs=JSONAnalyzer.composeJson(thought);
+
+        try{
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(connectionString+"thought.php");
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            is = entity.getContent();
+/*
+            obj.put("text","debugging response");//thought.getText());
+            obj.put("user","sadeem");//thought.getCreatedBy().getFirstName());
+            obj.put("anony","true");
+            obj.put("long","12212");//thought.getLocation().getLongitude());
+            obj.put("lat","1111");//thought.getLocation().getLatitude());
+            obj.put("temp","11");//thought.getTemperature().getTemperatureDegree());
+
+            HttpPost httpPost = new HttpPost("http://10.123.35.109/trojanNow/connect.php/");
+            httpPost.setEntity(new StringEntity(obj.toString()));
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+            new DefaultHttpClient().execute(httpPost);
+*/
+        }catch(Exception e){
+            System.out.println("log_tag Error in http connection" + e.toString());
+        }       // return false;
     }
 
     // This methods sends a directs message information to the server
-    public boolean sendDirectMessage(DirectMessage directMessage){
+    public void sendDirectMessage(DirectMessage directMessage){
         //TODO: fill in method skeleton
-        return false;
-    }
+        ArrayList<NameValuePair> nameValuePairs=JSONAnalyzer.composeJsonMsg(directMessage);
+
+        try{
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(connectionString+"message.php");
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+        }catch(Exception e){
+            System.out.println("log_tag Error in http connection" + e.toString());
+        }  }
 
         //This method calls the server to retrieve a JSON string that represents
         //the thoughts that are within specific range
-    public String getThoughts(LocationInfo location, float range){
-        //TODO: fill in method skeleton
-        return "";
+    public ArrayList<Thought> getThoughts(LocationInfo location, float range){
+        StringBuffer sb=null;
+        try
+        {
+
+            HttpClient client = new DefaultHttpClient();
+        HttpGet request = new HttpGet();
+        request.setURI(new URI(connectionString+"thought.php?thoughts=all"));
+        HttpResponse response = client.execute(request);
+        BufferedReader in = new BufferedReader
+                (new InputStreamReader(response.getEntity().getContent()));
+
+         sb = new StringBuffer("");
+        String line="";
+        while ((line = in.readLine()) != null) {
+            sb.append(line);
+            break;
+        }
+            in.close();
+            System.out.println("from localhost: "+sb.toString());
+                  return JSONAnalyzer.AnalyzeJson(sb.toString());
+
+        }catch(Exception e){
+        System.out.println("Exception: " + e.getMessage());
+    }return null;
     }
 
     //This method calls the server to retrieve a JSON string that represents
     //the inbox of a specific user
 
-    public String getMessages(User user){
-        //TODO: fill in method skeleton
-        return "";
+    public ArrayList<DirectMessage> getMessages(User user){
+        StringBuffer sb=null;
+        try
+        {
+
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet();
+            request.setURI(new URI(connectionString+"message.php?message="+user.getUserName()));
+            HttpResponse response = client.execute(request);
+            BufferedReader in = new BufferedReader
+                    (new InputStreamReader(response.getEntity().getContent()));
+
+            sb = new StringBuffer("");
+            String line="";
+            while ((line = in.readLine()) != null) {
+                sb.append(line);
+                break;
+            }
+            in.close();
+            System.out.println("from localhost: "+sb.toString());
+            return JSONAnalyzer.AnalyzeJsonMsg(sb.toString());
+
+        }catch(Exception e){
+            System.out.println("Exception: " + e.getMessage());
+        }return null;
     }
 
     //this methods connects to the server to login and create a session
     // for the user
     // it returns either "failed" or sessionID
-    public String logIn(String username, String passwd){
-        //TODO: fill in method skeleton
-        return "";
+    public User logIn(String username){
+        StringBuffer sb=null;
+        try
+        {
+
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet();
+            request.setURI(new URI(connectionString+"user.php?username="+username));
+            HttpResponse response = client.execute(request);
+            BufferedReader in = new BufferedReader
+                    (new InputStreamReader(response.getEntity().getContent()));
+
+            sb = new StringBuffer("");
+            String line="";
+            while ((line = in.readLine()) != null) {
+                sb.append(line);
+                break;
+            }
+            in.close();
+            System.out.println("from localhost: "+sb.toString());
+            return JSONAnalyzer.AnalyzeJsonUser(sb.toString());
+
+        }catch(Exception e){
+            System.out.println("Exception: " + e.getMessage());
+        }return null;
     }
 
     //this methods connects to the server to signup and create a new user
